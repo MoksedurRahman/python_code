87b5c44d72d45f543f4614157a9bc8ef23c45d47
Md Moksedur Rahman <moksedur.rahman@gmail.com>
2025-06-10 09:54:59 +0100

    Variable

---
+++ Python_Variables.py
@@ -0,0 +1,42 @@
+# Creating values
+
+x=10 # x is type of int
+y="Jhon"
+
+print(x)
+print(y)
+
+print("\nVariable type change\n---------")
+x="Ayyan" # x is type of str
+
+print(x)
+
+# Casting
+
+x=str(5)
+y = int(5)
+z = float(5)
+
+print("\nCasting \n--------")
+print(x)
+print(y)
+print(z)
+
+print("\nGet the Type\n--------")
+
+print(type(x))
+print(type(y))
+
+print("\nSingle Qoutes and Double Qoutes\n----------\n")
+
+m="Jhon"
+n="Rahman"
+print(m)
+print(n)
+
+print("\nCase sensitive\n")
+
+A="Rahman"
+a="Ayyan"
+print(A)
+print(a)
